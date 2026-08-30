eacc49e91f9dce2325a7a7688d8d641637a3f30a
gagakpink123 <gagakpink@gmail.com>
2026-08-30 17:14:53 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,7 @@ export default function App() {
   const [cameraError, setCameraError] = useState('');
 
   // ⚠️ GANTI URL INI JIKA ADA DEPLOYMENT GOOGLE APPS SCRIPT BARU
-  const [scriptUrl, setScriptUrl] = useState("https://script.google.com/macros/s/AKfycbzPz36vpkQU_TDt35yfixqThJeaNjiYgI7UOYsb4XdnEXX-6RJwamKsZJ72qIgCmUoJ/exec");
+  const [scriptUrl, setScriptUrl] = useState("https://script.google.com/macros/s/AKfycbyH36FxJxLJOmkR79Qj2osgF-jNXLBBfUiNAWqs2BvakGxhkW_0iwRBUJdyH1EXJU59/exec");
   
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [showSuccessModal, setShowSuccessModal] = useState(false);
